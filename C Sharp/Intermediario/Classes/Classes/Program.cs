f8c0755b1775d169f202ac9e081d475f2861423c
Bryan <mauriciobryan1998@gmail.com>
2022-03-04 10:20:25 -0300

    Enum Class

---
+++ C Sharp/Intermediario/Classes/Classes/Program.cs
@@ -0,0 +1,31 @@
+﻿using Classes.Entities;
+using Classes.Entities.Enums;
+using System;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Order order = new Order { 
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+           
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
