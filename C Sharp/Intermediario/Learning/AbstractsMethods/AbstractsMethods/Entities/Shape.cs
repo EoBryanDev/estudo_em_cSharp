445f651029db1a3dcf3b79a3300761b13e010340
Bryan <mauriciobryan1998@gmail.com>
2022-03-08 19:05:45 -0300

    Shapes_Exercise

---
+++ C Sharp/Intermediario/Learning/AbstractsMethods/AbstractsMethods/Entities/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractsMethods.Entities.Enums;
+
+namespace AbstractsMethods.Entities
+{
+    abstract internal class Shape
+    {
+        public Color Color { get; set; }
+        public Shape(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();
+    }
+}
