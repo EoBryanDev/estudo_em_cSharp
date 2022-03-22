041a4b6362c64a0c3a96ba55510a84b74c41819b
EoBryanDev <mauriciobryan1998@gmail.com>
2022-03-22 09:23:43 -0300

    EqualsGetHash Class

---
+++ C Sharp/Intermediario/Learning/EqualsGetHashClass/EqualsGetHashClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EqualsGetHashClass.Entities;
+
+
+namespace EqualsGetHashClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Client a = new Client("Maria","maria@gmail.com");
+            Client b = new Client("Alex", "alex@gmail.com");
+            Console.WriteLine(a.Equals(b));
+
+
+            Console.ReadKey();
+        }
+    }
+}
