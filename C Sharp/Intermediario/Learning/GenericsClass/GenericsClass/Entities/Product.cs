31ee8672bbc556f4fb0e8cf098ff42db78290525
EoBryanDev <mauriciobryan1998@gmail.com>
2022-03-22 09:08:12 -0300

    Generics Class Complete

--- C Sharp/Intermediario/Learning/GenericsClass/GenericsClass/Entities/Product.cs
+++ C Sharp/Intermediario/Learning/GenericsClass/GenericsClass/Entities/Product.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace GenericsClass.Entities
 {
-    internal class Product
+    internal class Product :IComparable
     {
-        
+     public string Name { get; set; }
+     public double Price { get; set; }
+
+    public Product(string name, double price)
+        {
+            Name = name; Price = price;
+        }
+        public override string ToString()
+        {
+            return $"{Name}, {Price.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Product))
+            {
+                throw new ArgumentException("Comparition can not be compared!");
+            }
+            Product other = obj as Product;
+            return Price.CompareTo(other.Price);
+        }
     }
 }
--- C Sharp/Intermediario/Learning/GenericsClass/GenericsClass/Program.cs
+++ C Sharp/Intermediario/Learning/GenericsClass/GenericsClass/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GenericsClass.Entities;
+using System.Globalization;
 
 namespace GenericsClass
 {
@@ -11,18 +13,21 @@ namespace GenericsClass
         static void Main(string[] args)
         {
 
-            List<int> list = new List<int>();
+            List<Product> list = new List<Product>();
 
             Console.Write("Enter N:");
             int n = int.Parse(Console.ReadLine());
 
             for(int i = 0; i < n; i++)
             {
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
                 int x = int.Parse(Console.ReadLine());
-                list.Add(x);
+                list.Add(new Product(name,price));
             }
             CalculationService calculationService = new CalculationService();
-            int max = calculationService.Max(list);
+            Product max = calculationService.Max(list);
 
             Console.WriteLine($"Max: {max}");
 
