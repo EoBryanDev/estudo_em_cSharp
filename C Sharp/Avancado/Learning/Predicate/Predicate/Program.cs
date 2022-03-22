63a41da4916e3a1b4607686fe417333d0d15b8f4
EoBryanDev <mauriciobryan1998@gmail.com>
2022-03-22 18:48:47 -0300

    PredicateClass

---
+++ C Sharp/Avancado/Learning/Predicate/Predicate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Predicate.Entities;
+
+
+namespace Predicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("MOUSE", 50.00));
+            list.Add(new Product("TABLET", 350.00));
+            list.Add(new Product("HD CASE", 80.90));
+
+            //remove using with lambda expression
+            //----------list.RemoveAll(p => p.Price >= 100.0);
+
+            //remove using predicate 
+            list.RemoveAll(ProductTest);
+
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+            Console.ReadKey();
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.00;
+        }
+    }
+}
