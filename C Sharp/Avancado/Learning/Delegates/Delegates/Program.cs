e94325dee60c37941465b78dfbf47e99b0691a9f
EoBryanDev <mauriciobryan1998@gmail.com>
2022-03-22 18:26:46 -0300

    Começo de aula de programação funcional

---
+++ C Sharp/Avancado/Learning/Delegates/Delegates/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Delegates.Services;
+
+namespace Delegates
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double a = 10;
+            double b = 12;
+
+            //i also could use
+            //BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+            //but it's so long.
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            // i also could use result = op.Invoke(a,b)
+            double result = op(a, b);
+            Console.WriteLine(result);
+            /*
+            List<double> list = new List<double>();
+
+            double result = CalculationService.Sum(a, b);
+            list.Add(result);
+            double result2 = CalculationService.Max(a, b);
+            list.Add(result2);
+            double result3 = CalculationService.Square(a);
+            list.Add(result3);
+
+            Console.Write("[ ");
+            foreach (double results in list)
+            {
+                Console.Write($"{results} ,");
+            }
+            Console.Write(" ] ");*/
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C Sharp/Intermediario/Learning/DictionaryClass/DictionaryClass/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,string> cookies = new Dictionary<string,string>();
+
+            cookies["user"] = "maria";
+
+            cookies["email"] = "maria@gmail.com";
+            //set some item in on dictionary
+            cookies["phone"] = "99999999";
+
+            cookies["phone"] = "11111111";
+
+
+            Console.WriteLine(cookies["phone"]);
+
+            //remove some item in on dictionary
+            cookies.Remove("phone");
+
+            Console.WriteLine($"Size : {cookies.Count}");
+            Console.WriteLine("All cookies: ");
+            // i can use keyvaluepair or var
+            foreach(KeyValuePair<string,string> item in cookies)
+            {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+
+            Console.ReadKey();
+
+        
+        }
+    }
+}
